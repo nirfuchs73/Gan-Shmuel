1df684dc75d306987ad545975524be6f460b1564
nirfuchs73 <nirfuchs@hotmail.com>
2019-12-22 18:31:16 +0200

    post from git

--- devops/ci-server/ci-server.py
+++ devops/ci-server/ci-server.py
@@ -8,7 +8,10 @@ def root():
 
 @app.route("/", methods=['POST'])
 def post_git():
-    return 'POST'  
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, threaded=True, port=8081)
