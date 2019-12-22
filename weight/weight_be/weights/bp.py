0c6f814f68582d20d990d5996c120dc101d4d7e4
Shlomo Kallner <shlomo.kallner@gmail.com>
2019-12-22 18:50:02 +0200

    Complete the DB Models1

--- weight/weight_be/weights/bp.py
+++ weight/weight_be/weights/bp.py
@@ -5,8 +5,11 @@
 import functools
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, 
+    render_template, request, session, url_for,
+    current_app, make_response, jsonify
 )
+# from flask.json import jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from . import db
@@ -17,6 +20,12 @@ def create_views_blueprint():
     @bp.route('/health', methods=['GET'])
     def health():
         cdb = db.get_db()
-        pass
+        res_t = cdb.show_tables()
+        if len(res_t) > 0:
+            # check the description of the db tables
+            return jsonify({'message':"OK", 'status':200})
+        else:
+            return jsonify({'message':"No Database Tables!", 'status':404})
+        
 
     return bp
---
+++ weight/weight_be/weights/db/models/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+
+from . import container
+from . import transaction
