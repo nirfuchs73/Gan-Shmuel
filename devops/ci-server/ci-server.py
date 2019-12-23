498591e123e85764a58bff4c8785045a9e064842
Hadas <hadasmazoozb@gmail.com>
2019-12-23 16:04:54 +0200

    deploy

--- devops/ci-server/ci-server.py
+++ devops/ci-server/ci-server.py
@@ -38,16 +38,21 @@ def post_git():
 
     send_notification(success)
 
-    if success and ref == 'refs/heads/master':
+    if success and branch == 'master':
         run_deploy()
 
     return 'JSON posted'
 
 
 def run_checkout(branch):
-    full_args = 'checkout '+ branch
-    result = run_process('git', full_args)
     print('run_checkout')
+    arguments = 'checkout '+ branch
+    command = 'git'
+    try:
+        if not run_process(command, arguments):
+            result = False
+    except:
+        result = False
     return result
 
 def run_build():
@@ -83,6 +88,28 @@ def send_notification(success):
 def run_deploy():
     result = True
     print('run_deploy')
+    docker_compose_file = os.path.join('../../', 'docker-compose.yml')
+    if os.path.exists(docker_compose_file):
+        command = 'docker-compose'
+        arguments = '--file ' + docker_compose_file + ' down -d '
+        try:
+            if not run_process(command, arguments):
+                result = False
+        except:
+            result = False
+
+        argumentsForUp = '--file ' + docker_compose_file + ' up -d '
+        try:
+            if not run_process(command, argumentsForUp):
+                result = False
+        except:
+            result = False
+
+    else:
+        print(docker_compose_file + ' does not exist')
+        result = False
+        return result
+
     return result
 
 
