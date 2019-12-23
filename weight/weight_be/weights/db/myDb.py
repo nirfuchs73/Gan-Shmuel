7b2e19c34900e06b184637a9303dcee00cf30358
Shlomo Kallner <shlomo.kallner@gmail.com>
2019-12-23 15:22:03 +0200

    Finish __doc__ strings in myDb.py:MyDb(), Add method commit() and call commit() in execute()

--- weight/weight_be/weights/db/myDb.py
+++ weight/weight_be/weights/db/myDb.py
@@ -14,6 +14,10 @@ class MyDb(object):
         self.__set_db(current_app)
         self.db.close()
 
+    def commit(self):
+        self.__set_db(current_app)
+        self.db.commit()
+
     @staticmethod
     def __get_db(app):
         try:
@@ -71,44 +75,88 @@ class MyDb(object):
 
     # a few general SQL command execution methods
 
-<<<<<<< HEAD
     def execute(self, query, params=[]):
         '''
             Run a non-"Select" or other Data Retrival Query with optional parameters.
             usage: 
-            cdb.execute("<some_query_string_>", [<some_list_or_tuple_of_parameters>])
-
-
+            'cdb.execute("<some_query_string_>", [<some_list_or_tuple_of_parameters>])'
+            Use "%s" in the query string where you want execute() to insert a parameter
+            if using a list or tuple for "params" 
+            (Note: it will insert them in the order they are passed into "params"!);
+            if using a dict for "params", use "%(<param_key>)s" 
+            where "<param_key>" is a key in the dict.
+            Parameter "params" MUST be a list , a tuple or a dict!
+            Throws a TypeError if "params" is not of the right type!
+            Returns the number of Rows "affected" / "inserted" on success.
+            Returns -1 (or None) on failure or if unable to determine the number
+            of Rows "affected" / "inserted".
+            Throws a mysql.connector.errors.Error on internal MySQL Error.
         '''
         cur = self.__get_cursor(current_app)
         try:
             cur.execute(query, self.__check__params(params))
+            res = cur.rowcount
             cur.close()
-            return cur.rowcount
+            self.commit()
+            return res
         except Error as e:
             raise
         except TypeError as t:
             raise
         
 
-=======
->>>>>>> master
     def execute_and_get_all(self, query, params=[]):
+        '''
+            Run a "Select" or other Data Retrival Query with optional parameters
+            and retrieve ALL results.
+            usage: 
+            'cdb.execute_and_get_all("<some_query_string_>", [<some_list_or_tuple_of_parameters>])'
+            Use "%s" in the query string where you want execute() to insert a parameter
+            if using a list or tuple for "params" 
+            (Note: it will insert them in the order they are passed into "params"!);
+            if using a dict for "params", use "%(<param_key>)s" 
+            where "<param_key>" is a key in the dict.
+            Parameter "params" MUST be a list , a tuple or a dict!
+            Throws a TypeError if "params" is not of the right type!
+            Returns a list of all Rows (each as a dict) on success.
+            Returns an empty list on failure.
+            Throws a mysql.connector.errors.Error on internal MySQL Error.
+        '''
         cur = self.__get_cursor(current_app)
         try:
-            cur.execute(query, params)
+            cur.execute(query, self.__check__params(params))
             res = cur.fetchall()
             cur.close()
             return res
         except Error as e:
             raise
+        except TypeError as t:
+            raise
 
     def execute_and_get_one(self, query, params=[]):
+        '''
+            Run a "Select" or other Data Retrival Query with optional parameters
+            and retrieve JUST ONE result.
+            usage: 
+            'cdb.execute_and_get_one("<some_query_string_>", [<some_list_or_tuple_of_parameters>])'
+            Use "%s" in the query string where you want execute() to insert a parameter
+            if using a list or tuple for "params" 
+            (Note: it will insert them in the order they are passed into "params"!);
+            if using a dict for "params", use "%(<param_key>)s" 
+            where "<param_key>" is a key in the dict.
+            Parameter "params" MUST be a list , a tuple or a dict!
+            Throws a TypeError if "params" is not of the right type!
+            Returns the Row (as a dict) on success.
+            Returns None on failure.
+            Throws a mysql.connector.errors.Error on internal MySQL Error.
+        '''
         cur = self.__get_cursor(current_app)
         try:
-            cur.execute(query, params)
+            cur.execute(query, self.__check__params(params))
             res = cur.fetchone()
             cur.close()
             return res
         except Error as e:
+            raise
+        except TypeError as t:
             raise
