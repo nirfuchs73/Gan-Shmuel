980844ab611b116ba976de1ba4e4ac64f97f40fd
nirfuchs73 <nirfuchs@hotmail.com>
2019-12-23 10:59:30 +0200

    ci server

--- devops/ci-server/ci-server.py
+++ devops/ci-server/ci-server.py
@@ -1,14 +1,16 @@
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def root():
     return 'root'
 
+
 @app.route("/", methods=['POST'])
 def post_git():
-    print (request.is_json)
+    print(request.is_json)
     data = request.get_json(force=True)
     # print (data)
     ref = data['ref']
@@ -22,8 +24,53 @@ def post_git():
     print(pusher)
     print(name)
     print(email)
-    
+
+    success = True
+
+    success = run_checkout(ref)
+    if success:
+        success = run_build()
+    if success:
+        success = run_tests()
+
+    send_notification(success)
+
+    if success and ref == 'refs/heads/master':
+        run_deploy()
+
     return 'JSON posted'
+
+
+def run_checkout(ref):
+    result = True
+    print('run_checkout')
+    return result
+
+
+def run_build():
+    result = True
+    print('run_build')
+    print('docker-compose up --build')
+    return result
+
+
+def run_tests():
+    result = True
+    print('run_test')
+    return result
+
+
+def send_notification(success):
+    result = True
+    print('send_notification')
+    return result
+
+
+def run_deploy():
+    result = True
+    print('run_deploy')
+    return result
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, threaded=True, port=8081)
