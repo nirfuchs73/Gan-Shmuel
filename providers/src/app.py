649d749606bb25b4c74e225ed928f4a0938cc631
Kim jong il <thekimi@live.com>
2019-12-23 14:11:32 +0200

    added get health

--- providers/src/app.py
+++ providers/src/app.py
@@ -1,18 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import requests
 import mysql.connector
 import csv
+import xlrd
 
 app = Flask(__name__)
 
-sql = mysql.connector.connect(user='root', password='12345678',
-                            host='localhost',
-                            database='db')
-cursor = sql.cursor()
+# Connect to the db database in the mysql container.
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="12345678",
+    auth_plugin='mysql_native_password',
+    database='db'
+)
+cursor = db.cursor()
 
 @app.route('/health', methods=['GET'])
 def health():
-    return "empty"
+    try:
+        cursor.execute("SELECT 1;")
+    except Exception as e:
+        return Response(status=500)
+    else:
+        return Response(status=200)
+    return
 
 @app.route('/provider', methods=['POST'])
 def provider():
@@ -20,16 +32,21 @@ def provider():
 
 @app.route('/rates', methods=['POST', 'GET'])
 def rates():
-    if request.method == 'POST':
-        path = "/in/" + str(request.form.get("file"))
-        # TODO save file in mysql volume?
-        with open(path, 'rb') as csvfile:
-            
-        cursor.execute('insert into test_table ...')
+
+    if request.method == 'GET':
         return "empty"
         
-    elif request.method == 'GET':
-        return "empty"
+    elif request.method == 'POST':
+        rf = request.form
+        path = "in/" + str(rf.get("file"))
+        # TODO save file in mysql volume?
+        wb = xlrd.open_workbook(path)
+        sheet = wb.sheet_by_index(0)
+        cursor.execute("delete from Rates;")
+        for i in range(1, sheet.nrows):
+            #print(str(sheet.row_values(i))[1:-1])
+            cursor.execute("INSERT INTO Rates VALUES (" + str(sheet.row_values(i))[1:-1] + ");")
+
     return "empy"
 
 @app.route('/truck', methods=['POST'])
@@ -41,15 +58,7 @@ def truck():
     truckid = request.post['id']
     providerid = request.form['provider']
 
-    # Connect to the db database in the mysql container.
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="12345678",
-        auth_plugin='mysql_native_password',
-        database='db'
-    )
-    cursor = db.cursor()
+    
 
     # Insert the truck data in to the trucks table.
     cursor.execute(f"INSERT INTO trucks (truckid, providerid) VALUES ('{truckid}', '{providerid}');")
@@ -62,6 +71,7 @@ def bill():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
-conn.close()
+cursor.close()
+db.close()
