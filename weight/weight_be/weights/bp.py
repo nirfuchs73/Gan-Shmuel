20b1ae31e89ddf0e2e49070d6ae76341fde97417
Gili <gilad.w94@gmail.com>
2019-12-23 15:56:47 +0200

    add POST/batch-weight

--- weight/weight_be/weights/bp.py
+++ weight/weight_be/weights/bp.py
@@ -4,6 +4,11 @@
 # global (system) imports
 import functools
 
+#add by gilad for batch_weight
+import os
+import csv
+#add by gilad for batch_weight
+
 from flask import (
     Blueprint, flash, g, redirect, 
     render_template, request, session, url_for,
@@ -26,6 +31,49 @@ def create_views_blueprint():
             return jsonify({'message':"OK", 'status':200})
         else:
             return jsonify({'message':"No Database Tables!", 'status':404})
+
+    @bp.route('/batch-weight', methods=['POST'])
+    def batch-weight():
+        title = "Batch"
+        cdb = db.get_db()
+        #dealing with a json/csv file
+        jsonFile = "in/containers3.json"
+        csvFile = "in/container.csv"
+        if os.path.exists(jsonFile):
+            file_data = ""
+            try:
+                with open(jsonFile) as f:
+                    file_data = f.read()
+                    query = "DELETE * FROM container_registered"
+                    cdb.execut(query)
+                    query = "INSERT INTO container_registered SELECT * FROM" 
+                    cdb.execut(query,[file_data])  
+            except:
+                return jsonify({'message':"could not read file!", 'status':404})
+        elif os.path.exists(csvFile):
+            query = "DELETE * FROM container_registered"
+            cdb.execut(query)
+            file_data = ""
+            first_line_check=0
+            try:
+                with open(csvFile) as f:
+                    file_data = f.read().splitlines(True)
+                    for line in file_data
+                        lineSplit = line.split(',')
+                        if first_line_check == 0:              #checks the first line if the data is in kg/lbs
+                        head_lines = file_data[1]
+                        first_line_check+=1
+                        else:
+                            if head_lines == 'kg' or head_lines == 'lbs': #could add functionality for capital letters 
+                                query = "INSERT INTO container_registered VALUES (%s, %s, %s);"
+                                cdb.execut(query,[lineSplit[0],lineSplit[1], head_lines])
+                            else:
+                                return jsonify({'message':"no specified data (kg,lbs)", 'status':404})
+            except:
+                return jsonify({'message':"could not read file!", 'status':404})
+        else:
+            return jsonify({'message':"found no existing file!", 'status':404})
+        #res_t = cdb.show_tables()
         
 
     return bp
