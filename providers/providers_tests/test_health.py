8ab543c8c65f5c448030d8edf23f3d4d834667c6
Hadas <hadasmazoozb@gmail.com>
2019-12-25 13:07:36 +0200

    providers-test

--- providers/providers_tests/test_health.py
+++ providers/providers_tests/test_health.py
@@ -5,7 +5,7 @@ import requests
 class FlaskTestCase(unittest.TestCase):
     def test_health(self):
         print('health_test')
-        URL = "http://localhost:8080/health"
+        URL = "http://ec2-54-237-240-191.compute-1.amazonaws.com:8080/health"
         response = requests.get(url=URL)
         self.assertEqual(response.status_code, 200)
 
