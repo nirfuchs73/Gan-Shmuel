7d0b6554bd696db5c9fe9f27b4475bf06584778a
idan secret <idan441@gmail.com>
2019-12-25 18:09:50 +0200

    working - 2 from 8 failrus - 6 succeded

--- providers/src/test_health.py
+++ providers/src/test_health.py
@@ -13,54 +13,54 @@ def main(out = sys.stderr, verbosity = 2):
 
 class FlaskTestCase(unittest.TestCase):
     def test_health(self):
-        URL = f"http://localhost:8080/health"
+        URL = f"http://localhost:8082/health"
 
         response = requests.get(url=URL)
         self.assertEqual(response.status_code, 200)
 
 
     def test_provider(self):
-        URL = f"http://localhost:8080/provider"
+        URL = f"http://localhost:8082/provider"
 
         response = requests.post(url=URL, data={'name': 'test_please_remove'})
         self.assertEqual(response.status_code, 200)
 
 
     def test_rates_post(self):
-        URL = f"http://localhost:8080/rates"
+        URL = f"http://localhost:8082/rates"
 
         response = requests.post(url=URL, data={'file': 'test_please_delete'})
         self.assertEqual(response.status_code, 200)
 
 
     def test_rates_get(self):
-        URL = f"http://localhost:8080/rates"
+        URL = f"http://localhost:8082/rates"
 
         response = requests.get(url=URL)
         self.assertEqual(response.status_code, 200)
 
 
     def test_truck_put(self):
-        URL = f"http://localhost:8080/truck?truck_id=77777&provider_id=2"
+        URL = f"http://localhost:8082/truck?truck_id=77777&provider_id=2"
 
         response = requests.put(url=URL, data={'truck_id': '77777', 'provider_id': 2})
         self.assertEqual(response.status_code, 200)
 
 
     def test_truck_post(self):
-        URL = f"http://localhost:8080/truck"
+        URL = f"http://localhost:8082/truck"
 
         response = requests.post(url=URL, data={'id': 'test', 'provider': 0})
         self.assertEqual(response.status_code, 200)
 
     def test_truck_get(self):
-        URL = f"http://localhost:8080/truck"
+        URL = f"http://localhost:8082/truck"
 
         response = requests.get(url=URL, data={'from': '200001010000', 'to': '205001010000'})
         self.assertEqual(response.status_code, 200)
 
     def test_bill_get(self):
-        URL = f"http://localhost:8080/bill"
+        URL = f"http://localhost:8082/bill"
 
         response = requests.get(url=URL, data={'first_name': 'test'})
         self.assertEqual(response.status_code, 200)
