7e7f585f7d31c83bd1d0ef9953c04156968605c1
nirfuchs73 <nirfuchs@hotmail.com>
2019-12-25 15:58:02 +0200

    network_mode: host

--- weight/weight_tests/test_health.py
+++ weight/weight_tests/test_health.py
@@ -5,7 +5,7 @@ import requests
 class FlaskTestCase(unittest.TestCase):
     def test_health(self):
         print('health_test')
-        URL = "http://weight_be_test:8089/health"
+        URL = "http://localhost:8089/health"
         response = requests.get(url=URL,verify=False)
         self.assertEqual(response.status_code, 200)
 
