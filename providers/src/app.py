e682b8f3b14e7c20b927947eb3157652ed203ac7
geco333 <geco333@gmail.com>
2019-12-25 12:06:42 +0200

    Fixed truck_get function in app.py

--- providers/src/app.py
+++ providers/src/app.py
@@ -151,12 +151,15 @@ def truck_get(truckid):
      1st of the month to the current date.
      Returns 404 if the database does not contain trucks between the specified dates"""
 
-    _from = datetime.now().strftime('%Y%m01000000')
-    _to = datetime.now().strftime('%Y%m%d%H%M%S')
+    _from = request.args['from']
+    _to = request.args['to']
 
-    item = requests.get(f'localhost:8090/unit/{truckid}', {'from': _from, 'to': _to})
+    if _from == datetime.now().strftime('%Y%m01000000') and _to == datetime.now().strftime('%Y%m%d%H%M%S'):
+        item = requests.get(f'localhost:8090/item/{truckid}', {'from': _from, 'to': _to})
 
-    return item, 200
+        return item, 200
+    else:
+        return '', 404
 
 
 @app.route('/bill', methods=['GET'])
