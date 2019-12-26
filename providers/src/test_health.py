56e1efd49f465a28241e85a753d8b0ca9a7b20bd
geco333 <geco333@gmail.com>
2019-12-26 14:34:51 +0200

    Fixed index.html

--- providers/src/test_health.py
+++ providers/src/test_health.py
@@ -1,15 +1,15 @@
 import unittest
 import requests
 
+import sys
 
-import sys 
-  
-def main(out = sys.stderr, verbosity = 2): 
-    loader = unittest.TestLoader() 
-  
-    suite = loader.loadTestsFromModule(sys.modules[__name__]) 
-    unittest.TextTestRunner(out, verbosity = verbosity).run(suite) 
-    
+
+def main(out=sys.stderr, verbosity=2):
+    loader = unittest.TestLoader()
+
+    suite = loader.loadTestsFromModule(sys.modules[__name__])
+    unittest.TextTestRunner(out, verbosity=verbosity).run(suite)
+
 
 class FlaskTestCase(unittest.TestCase):
     def test_health(self):
@@ -50,14 +50,16 @@ class FlaskTestCase(unittest.TestCase):
     def test_truck_post(self):
         URL = f"http://localhost:8082/truck"
 
-        response = requests.post(url=URL, data={'id': 'test', 'provider': 0})
+        response = requests.post(url=URL, data={'id': 'test', 'provider_id': 0})
         self.assertEqual(response.status_code, 200)
+
 
     def test_truck_get(self):
         URL = f"http://localhost:8082/truck"
 
         response = requests.get(url=URL, data={'from': '200001010000', 'to': '205001010000'})
         self.assertEqual(response.status_code, 200)
+
 
     def test_bill_get(self):
         URL = f"http://localhost:8082/bill"
@@ -66,8 +68,7 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-
-if __name__ == '__main__': 
-    with open('testing.out', 'w') as f: 
-        main(f) 
-        #unittest.main(verbosity=2)
+if __name__ == '__main__':
+    with open('testing.out', 'w') as f:
+        main(f)
+        # unittest.main(verbosity=2)
